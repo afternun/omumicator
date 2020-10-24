99d3fccfcb7fb105740f9bf380fe129ed26b1019
Michał Bałos <michal@MacBook-Pro-Micha.local>
2020-10-24 12:49:02 +0200

    Initial commit

---
+++ src/main/java/com/afternun/omumicator/OmumicatorApplication.java
@@ -0,0 +1,13 @@
+package com.afternun.omumicator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmumicatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmumicatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/afternun/omumicator/OmumicatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afternun.omumicator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmumicatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
